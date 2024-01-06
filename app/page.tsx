e4226ad0ce788d00fac4d5b79cf33e28c6ccf0d0
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2024-01-06 12:24:03 +0530

    clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
-    <div>hhh</div>
+    <div className='h-screen'>
+      <Button>Click me</Button>
+      <UserButton afterSignOutUrl="/"/>
+      
+    </div>
   )
 }
